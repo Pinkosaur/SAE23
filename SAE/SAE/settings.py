e393f246adbf08f9849832a7b4ff34274270d40e
Pinkosaur <gaetan.thomassin@gmail.com>
2023-06-04 16:54:16 +0200

    :'(

--- SAE/SAE/settings.py
+++ SAE/SAE/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ludotheque.apps.LudothequeConfig'
+    'ludotheque.apps.LudothequeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
