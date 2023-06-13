ac5b419a0ddb0531b55db136773bd115db2b3a86
Pinkosaur <gaetan.thomassin@gmail.com>
2023-06-13 18:25:34 +0200

    la souffrance

--- SAE/ludotheque/views_jeu.py
+++ SAE/ludotheque/views_jeu.py
@@ -17,8 +17,7 @@ def traitementJeu(request):
 
 def afficheJeu(request, id):
     jeu = models.Jeu.objects.get(pk=id)
-    image = jeu.photoJeu
-    return render(request, 'ludotheque/jeux/afficheJeu.html', {'jeu': jeu, 'image':image})
+    return render(request, 'ludotheque/jeux/afficheJeu.html', {'jeu': jeu})
 
 def updateJeu(request, id):
     jeu = models.Jeu.objects.get(pk=id)
@@ -32,16 +31,23 @@ def updatetraitementJeu(request, id):
         jeu = jeuform.save(commit = False)
         jeu.id = saveid
         jeu.save()
-        image = jeu.photoJeu
-        return render(request, "ludotheque/jeux/afficheJeu.html", {"jeu": jeu, 'image':image})
+        return render(request, "ludotheque/jeux/afficheJeu.html", {"jeu": jeu})
     else:
         return render(request, "ludotheque/jeux/updateJeu.html", {"form": jeuform})
 
 def deleteJeu(request, id):
     suppr = models.Jeu.objects.get(pk=id)
     suppr.delete()
-    return HttpResponseRedirect("/ludotheque/indexJeu")
+    return HttpResponseRedirect("/ludotheque/indexJeu/")
 
 def indexJeu(request):
     liste = models.Jeu.objects.all()
-    return render(request, "ludotheque/jeux/indexJeu.html", {"liste": liste})
+    """note_moyenne = 0
+    diviseur = 0
+    notes = models.Comm.objects.filter(jeuComm_id=liste.id)
+    for i in notes.noteComm:
+        note_moyenne += i
+        diviseur +=1
+    note_moyenne/=diviseur"""
+    return render(request, "ludotheque/jeux/indexJeu.html", {"liste": liste,# 'note_moyenne':note_moyenne
+                  })
